077f02700c0eb5941915dd2173bb8b6fc5d05ec7
svasenkov <aanher@gmail.com>
2020-08-28 23:22:08 +0300

    updated @Test annotation

--- src/test/java/tests/AmeriFlexTests.java
+++ src/test/java/tests/AmeriFlexTests.java
@@ -1,7 +1,8 @@
 package tests;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
